f4bd60862db44d360934fd3817045b8b5034da24
Manoj Mahato <127175133+Manoj-Mahat0@users.noreply.github.com>
2025-05-02 15:51:14 +0530

    Update database.py
    db

--- database.py
+++ database.py
@@ -2,8 +2,13 @@ from sqlalchemy import create_engine
 from sqlalchemy.ext.declarative import declarative_base
 from sqlalchemy.orm import sessionmaker
 
-DATABASE_URL = "mysql+pymysql://sql12774928:vFpTQfkMGd@sql12.freesqldatabase.com:3306/sql12774928"
+# Use a local SQLite file named 'app.db' in the same directory
+DATABASE_URL = "sqlite:///./app.db"
 
-engine = create_engine(DATABASE_URL)
+# Connect with SQLite
+engine = create_engine(
+    DATABASE_URL, connect_args={"check_same_thread": False}
+)
+
 SessionLocal = sessionmaker(autocommit=False, autoflush=False, bind=engine)
 Base = declarative_base()
